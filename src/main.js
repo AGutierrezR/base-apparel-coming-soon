863a1f6d16d59812bc5dd232e0f74f67fb86c63d
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-05-16 11:41:59 +0200

    Add form validation

--- src/main.js
+++ src/main.js
@@ -3,12 +3,50 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
-Alpine.data('flyout', () => ({
-  open: false,
+document.addEventListener('alpine:init', () => {
+  Alpine.data('form', () => ({
+    email: {
+      value: '',
+      valid: true,
+      message: '',
+    },
 
-  toggle() {
-    this.open = !this.open
-  },
-}))
+    validateForm() {
+      const email = this.email.value.trim()
+
+      console.log({ value: email, obj: this.email })
+
+      if (email === '') {
+        this.setErrorFor(this.email, 'Email cannot be empty')
+        return
+      }
+
+      this.removeErrorFor(this.email)
+
+      this.validate(this.email, 'Email')
+    },
+
+    setErrorFor(field, message) {
+      field.valid = false
+      field.message = message
+    },
+
+    removeErrorFor(field) {
+      field.valid = true
+      field.message = ''
+    },
+
+    validate(field, label) {
+      console.log('a ver')
+      if (!/.+\@.+\..+/.test(field.value) && label === 'Email') {
+        field.valid = false
+        field.message = 'Please provide a valid email'
+      } else {
+        field.valid = true
+        field.message = ''
+      }
+    },
+  }))
+})
 
 Alpine.start()
